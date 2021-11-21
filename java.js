eaaa7de2b9c557ec7ed3ce36d287fcad18dae92c
chris howard <christopherhoward1997@gmail.com>
2021-11-21 16:10:03 -0700

    questions made

---
+++ java.js
@@ -0,0 +1,64 @@
+const letsGo = document.getElementById("btn-home")
+const highScore = document.getElementById("highscore-btn")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions = [
+   {
+       questions: 'How fast was the first person convicted of speeding going?'
+       answers: [
+           {text: '8mph', correct: true}
+           {text: '25mph', correct: false}
+           {text: '18mph', correct: false}
+           {text: '5mph', correct: false}
+        ]
+   },
+   {
+    questions: 'What color is a polar bears skin?'
+    answers: [
+        {text: 'white', correct: true}
+        {text: 'pink', correct: false}
+        {text: 'black', correct: false}
+        {text: 'skin', correct: false}
+     ]
+},
+{
+    questions: 'Where is a shrimps heart located?'
+    answers: [
+        {text: 'butt', correct: true}
+        {text: 'head', correct: false}
+        {text: 'chest', correct: false}
+        {text: 'abdominal', correct: false}
+     ]
+},
+{
+    questions: 'What shape is a Wombat poop?'
+    answers: [
+        {text: 'circle', correct: true}
+        {text: 'cubed', correct: false}
+        {text: 'square', correct: false}
+        {text: 'triangle', correct: false}
+     ]
+},
+{
+    questions: 'How many muscles in a cats ear?'
+    answers: [
+        {text: '15', correct: true}
+        {text: '27', correct: false}
+        {text: '32', correct: false}
+        {text: '13', correct: false}
+     ]
+},
+]
